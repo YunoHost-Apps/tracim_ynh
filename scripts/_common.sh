d131730b5be090db1bd9ae9be07b21c027e4d516
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-04-27 22:55:37 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,13 +5,13 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql ghostscript git imagemagick iproute2 libfile-mimeinfo-perl libimage-exiftool-perl webp libmagic1 locales poppler-utils python3 python3-pip uwsgi uwsgi-plugin-python3 supervisor redis-server"
+#REMOVEME? pkg_dependencies="postgresql ghostscript git imagemagick iproute2 libfile-mimeinfo-perl libimage-exiftool-perl webp libmagic1 locales poppler-utils python3 python3-pip uwsgi uwsgi-plugin-python3 supervisor redis-server"
 
-preview_pkg_dependencies="ffmpeg xvfb inkscape libreoffice"
+#REMOVEME? preview_pkg_dependencies="ffmpeg xvfb inkscape libreoffice"
 
-pkg_dependencies="$pkg_dependencies $preview_pkg_dependencies"
+#REMOVEME? pkg_dependencies="$pkg_dependencies $preview_pkg_dependencies"
 
-build_pkg_dependencies="build-essential libjpeg-dev libmagickwand-dev libpq-dev python3-dev python3-venv"
+#REMOVEME? build_pkg_dependencies="build-essential libjpeg-dev libmagickwand-dev libpq-dev python3-dev python3-venv"
 
 nodejs_version=14
 
